ba936a86faa7851782e88d1442c7c8de7a3c456c
Abe Pernicka <Abe Pernicka>
2020-05-11 22:29:30 -0400

    Make it work for arbitrary length hashes

--- trans.js
+++ trans.js
@@ -5,23 +5,39 @@ const hash = process.argv[2];
 
 const animals = fs.readFileSync(path.resolve(__dirname, 'animals-f.txt')).toString();
 const adjectives = fs.readFileSync(path.resolve(__dirname, 'adjectives-f.txt')).toString();
-const animalList = animals.split('\n');
-const adjectiveList = adjectives.split('\n');
+const animalList = animals.split('\n').map(a => a.trimEnd());
+const adjectiveList = adjectives.split('\n').map(a => a.trimEnd());
 
-const parts = [];
-for (let i = 0; i < (hash.length / 2); i++) {
-  let part;
-  if (hash.length > (i * 2) + 1) {
-    part = `${hash[(i * 2)]}${hash[(i * 2) + 1]}`;
+if (!hash || hash.length === 0) {
+  console.log(`Usage: node trans.js <hash>`);
+  return;
+}
+try {
+  parseInt(hash, 16);
+} catch (err) {
+  console.log(`Usage: node trans.js <hash>`);
+  return;
+}
+
+const hashChunks = Math.floor(hash.length / 2);
+const lastChunkOneDigit = hashChunks % 2;
+let runningSum = 0;
+const partVals = [];
+for (let i = 0; i < hashChunks; i++) {
+  const lastChunk = (i === hashChunks - 1);
+  let hashPart;
+  if (!lastChunk || !lastChunkOneDigit) {
+    hashPart = `${hash[i * 2]}${hash[(i * 2) + 1]}`;
   } else {
-    part = `${hash[(i * 2)]}`;
+    hashPart = `${hash[i * 2]}`;
   }
-  const partVal = parseInt(part, 16);
-  if (hash.length > (i * 2) + 2) {
-    parts.push(adjectiveList[partVal + (256 * i)]);
+  const partVal = parseInt(hashPart, 16);
+  runningSum += partVal;
+  if (!lastChunk) {
+    partVals.push(adjectiveList[(runningSum % adjectiveList.length)]);
   } else {
-    parts.push(animalList[partVal]);
+    partVals.push(animalList[runningSum % animalList.length]);
   }
 }
-console.log(parts.join(''));
 
+console.log(partVals.join(''));
